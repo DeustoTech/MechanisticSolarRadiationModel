b722b1206ca707b1d180bf8a60b1abaede90ae9a
Jesus Oroya <41575404+djoroya@users.noreply.github.com>
2022-06-02 10:34:55 +0200

    update vectorize

--- DateTime2Rad.m
+++ DateTime2Rad.m
@@ -1,29 +1,11 @@
 function rad = DateTime2Rad(LocalTimes,Longitud,Latitud,DGMT)
 
 
-iter = 0;
-rad = zeros(size(LocalTimes));
-for iLT = LocalTimes
-    iter = iter + 1;
-    rad(iter) = DateTime2RadIter(iLT,Longitud,Latitud,DGMT);
-end
+theta = DateTime2Zenith(LocalTimes,Longitud,Latitud,DGMT) ;
+    
+rad =   (theta <= 90).*(1e3*1.353*0.68.^((1./cos(theta*(pi/180))).^1.5));
 
 
 end
 %%
-function rad = DateTime2RadIter(DateTime,Longitud,Latitud,DGMT)
 
-
-    theta = DateTime2Zenith(DateTime,Longitud,Latitud,DGMT) ;
-    
-    if theta > 90
-        rad = 0;
-    else
-        AM = 1/cos(theta*(pi/180));
-        %rad = 1e3*1.353*0.7^(AM^2.1);
-        %rad = 1e3*1.353*0.70^(AM^0.678);
-        rad = 1e3*1.353*0.68^(AM^1.5);
-
-    end
-
-end
--- DateTime2Zenith.m
+++ DateTime2Zenith.m
@@ -2,12 +2,8 @@ function theta = DateTime2Zenith(LocalTime,Longitud,Latitud,DGMT)
     %
     angle = DateTime2Elevation(LocalTime,Longitud,Latitud,DGMT);
 
+    theta = (angle>0).*(90 - angle)  + (angle<=0).*(90 + abs(angle));
         
-    if angle > 0
-        theta = 90 - angle ;
-    elseif angle <= 0
-        theta =  90+abs(angle);
-    end
     
 end
 
